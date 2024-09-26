adbd46f59927fc68768b7a2ca890e9353e232924
Julio M Cruz <julio.cruz@eb-ms.net>
2024-09-26 09:57:05 -0400

    Hacker House menu and landing components

---
+++ app/home-house/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import MenuHouseHeaderComponent from "@/components/MenuHouseHeader";
+import HomeHouseComponent from "@/components/HomeHouse";
+
+export default function HomeHouse() {
+  return (
+    <>
+      <MenuHouseHeaderComponent />
+      <HomeHouseComponent />
+    </>
+  );
+}
---
+++ components/HomeHouse.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from 'react';
+import { Button } from "@/components/ui/button"
+import { useRouter } from 'next/navigation';
+import HackerHouseListComponent from './HackerHouseList';
+
+export default function HomeHouseComponent() {
+
+  const router = useRouter();
+
+  const goToProofs = () => {
+    router.push('/user-proofs'); 
+  }; 
+
+  return (
+    <div className="relative min-h-screen w-full overflow-hidden flex flex-col items-center justify-between" style={{ minHeight: 'calc(100vh - 80px)' }}>
+
+      {/* Content */}
+
+      <div className="relative z-10 flex flex-col items-center mt-8 flex-grow w-full max-w-md px-6">
+
+        <HackerHouseListComponent />
+
+        <Button 
+          className="w-full max-w-[230px] my-4 bg-[#ff671e] hover:bg-orange-500 text-black text-xl py-8 rounded-xl shadow-xl border border-gray-600"
+          onClick={goToProofs}
+        >
+          Create Hacker House
+        </Button>
+        
+      </div>
+
+      {/* Version number */}
+      <div className="relative z-10 mb-4">
+        <p className="text-xs text-gray-800">v.0.01a</p>
+      </div>
+    </div>
+  );
+}
