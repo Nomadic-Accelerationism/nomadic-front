bf17f4fce4a467829ff6f8a94a47e6289b7cb70f
Julio M Cruz <julio.cruz@eb-ms.net>
2024-10-30 21:14:30 -0400

    Update components

--- app/journey-create/page.tsx
+++ app/journey-create/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import MenuUserHeaderComponent from "@/components/MenuUserHeader";
-import CreateJourneyComponent from "@/components/CreateJourney";
+import JourneyCreateComponent from "@/components/JourneyCreate";
 
 export default function HomeHouse() {
   return (
     <>
       <MenuUserHeaderComponent />
-      <CreateJourneyComponent />
+      <JourneyCreateComponent />
     </>
   );
 }
